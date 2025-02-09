faed318567b799cd426a672b30144c051ec6bf65
Jacob Quant <jacobq@gmail.com>
2025-02-09 12:28:16 -0600

    refactor: move settings from WS_Information.h to project_config.h

--- lib/waveshare-6ch-relay/include/WS_Information.h
+++ lib/waveshare-6ch-relay/include/WS_Information.h
@@ -1,23 +1,12 @@
 #ifndef _WS_INFORMATION_H_
 #define _WS_INFORMATION_H_
 
-#define WIFI_Enable           1  // Whether WIFI is enabled  .1:true    0:false
-#define Extension_Enable      0  // Whether to extend the connection to external devices   1:Expansion device Modbus RTU Relay    0:No extend
-#define RTC_Enable            0
+#define PROJECT_CONFIGURATION_HEADER_FILE "../../../project_config.h"
 
-
-// TODO: Replace with your wifi info
-// Name and password of the WiFi access point
-#define STASSID       "Waveshare-TL-WR886N_2L"
-#define STAPSK        "waveshare0755"
-
-// !!! Timing switch relay, this operation must be connected to the Pico-RTC-DS3231 with the correct time setting,
-// RTC_Enable must be set to 1. If no change is required, set RTC_Enable to 0
-/* Set in WS_RTC.h instead
-#define RTC_OPEN_Time_Hour    18              // Time when the timing switch is enabled
-#define RTC_OPEN_Time_Min     33              // Time when the timing switch is enabled
-#define RTC_Closs_Time_Hour   18              // Time when the timing switch is off
-#define RTC_Closs_Time_Min    34              // Time when the timing switch is off
-*/
+#if __has_include(PROJECT_CONFIGURATION_HEADER_FILE)
+#include PROJECT_CONFIGURATION_HEADER_FILE
+#else
+#error "You need to create a configuration file (e.g., " PROJECT_CONFIGURATION_HEADER_FILE ")"
+#endif
 
 #endif // _WS_INFORMATION_H_
